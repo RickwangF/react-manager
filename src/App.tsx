79de4461ea279205bf422429c3eef31b1f4c9092
王维余 <wangweiyu@9ji.com>
2025-09-20 18:37:09 +0800

    add BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {NavLink} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +10,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <NavLink to='/vite' target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        </NavLink>
+        <NavLink to='/react' target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </NavLink>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,25 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function React() {
+  return <h1>React Component</h1>
+}
+
+function Vite() {
+  return <h1>React Component</h1>
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}></Route>
+        <Route path='/react' element={<React/>}></Route>
+        <Route path='/vite' element={<Vite/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
 
