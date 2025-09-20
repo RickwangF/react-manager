4c5a5f130fc9799491034f3d427c3e1a7567f800
王维余 <wangweiyu@9ji.com>
2025-09-20 14:46:15 +0800

    add useMemo useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import { useState } from 'react'
+import {memo, useCallback, useMemo, useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Child = memo(( { onClick }: any ) => {
+  console.log('child called')
+  return (
+    <div>
+      <p>
+        我是一个Child节点
+        <button onClick={onClick}>我是一个Child的按钮</button>
+      </p>
+    </div>
+  )
+})
+
+
 function App() {
   const [count, setCount] = useState(0)
+
+  const handleButtonClicked = () => {
+    setCount(count + 1)
+  }
+
+  const total = () => {
+    console.log('total called')
+    const list = [1, 2, 3, 4, 5]
+    return list.reduce((a, b) => a + b, 0)
+  }
+
+  const total2 = useMemo(
+    () => {
+      console.log('total2 called')
+      const list = [1, 2, 3, 4, 5]
+      return list.reduce((a, b) => a + b, 0)
+    }
+  , [])
+
+  const handleChildClick  = useCallback(() => {
+    console.log('child clicked')
+  }, []);
 
   return (
     <>
@@ -18,16 +53,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={handleButtonClicked}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>{total()}</p>
+        <p>{total2}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Child onClick={handleChildClick}/>
     </>
   )
 }
